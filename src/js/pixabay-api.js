99d6ceddaa5710137bd43271b417c6164725a71d
Sapper <saniamykhailenko@gmail.com>
2025-07-11 21:19:49 +0300

    initial repo

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,20 @@
+// import flatpickr from "flatpickr";
+// import "flatpickr/dist/flatpickr.min.css";
+
+// import iziToast from "izitoast";
+// import "izitoast/dist/css/iziToast.min.css";
+
+// import icon from '../img/error.svg'
+
+
+
+
+
+/**
+ * getImagesByQuery(query). Ця функція повинна приймати один параметр query (пошукове слово, яке є рядком), 
+ * здійснювати HTTP-запит і повертати значення властивості data з отриманої відповіді.
+ */
+
+function getImagesByQuery(query) {
+
+}
---
+++ src/js/render-functions.js
@@ -0,0 +1,52 @@
+// import iziToast from "izitoast";
+// import "izitoast/dist/css/iziToast.min.css";
+
+
+/**
+ * createGallery(images). Ця функція повинна приймати масив images, створювати HTML-розмітку для 
+ * галереї, додавати її в контейнер галереї та викликати метод екземпляра SimpleLightbox refresh(). 
+ * Нічого не повертає.
+ */
+
+function createGallery(images) {
+
+}
+
+
+
+
+/**
+ * clearGallery(). Ця функція нічого не приймає та повинна очищати вміст контейнера галереї. 
+ * Нічого не повертає.
+ */
+
+
+
+function clearGallery() {
+
+}
+
+
+
+
+/**
+ * showLoader(). Ця функція нічого не приймає, повинна додавати клас для відображення лоадера. 
+ * Нічого не повертає.
+ */
+
+
+function showLoader() {
+
+}
+
+
+
+/**
+ * hideLoader(). Ця функція нічого не приймає, повинна прибирати клас для відображення лоадера. 
+ * Нічого не повертає.
+ */
+
+
+function hideLoader() {
+
+}
